dee6869d72e0791a1cfdd4967b5bd46d218c2bf3
rawan ahmad fawzi khasawneh <rawanahmed115@gmail.com>
2021-06-22 00:40:24 +0300

    problem solve

--- server.js
+++ server.js
@@ -3,12 +3,17 @@ const app = express()
 require('dotenv').config();
 const PORT = process.env.PORT;
 const mongoose = require('mongoose');
+const seedUserData = require('./models/user.model');
+const getBooks = require('./controller/books.controller')
 
 const cors = require('cors'); // enable the communication between the frontend and the backend
 
-app.use(cors())
-mongoose.connect('mongodb://localhost:3032')
+app.use(cors());
+mongoose.connect('mongodb://localhost:27017/myFavouriteBooks', 
+ { useNewUrlParser: true, useUnifiedTopology: true });
 
+//  seedUserData();
+ app.get('/books', getBooks)
 
 app.listen(PORT, () => {
     console.log(`Server started on ${PORT}`);
